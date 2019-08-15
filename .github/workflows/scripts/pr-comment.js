914035e5ef5c301512f452dcdea8b91d27652fdb
Gil Greenberg <ggreenberg@somethingdigital.com>
2019-08-14 23:10:07 -0400

    testing file logging

--- .github/workflows/scripts/pr-comment.js
+++ .github/workflows/scripts/pr-comment.js
@@ -1,4 +1,5 @@
 const comment = require('github-comment');
+const fs = require('fs');
 require('dotenv').config();
 
 const {
@@ -8,8 +9,14 @@ const {
 } = process.env;
 
 console.log(GITHUB_EVENT_PATH);
+fs.readFile(__dirname + GITHUB_EVENT_PATH, function (err, data) {
+    if (err) {
+        throw err;
+    }
+    console.log(data.toString());
+});
 // get file and show contents
 
-comment(GITHUB_AUTH_TOKEN, GITHUB_REPOSITORY, 17, 'This is a mother fucking comment.')
+comment(GITHUB_AUTH_TOKEN, GITHUB_REPOSITORY, 18, 'This is a mother fucking comment.')
     .then(response => console.log(response))
     .catch(error => console.log(error))
