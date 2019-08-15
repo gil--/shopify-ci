2df3bb5d9282f867e5ca54cb49ca6fcdb8a788d3
Gil Greenberg <ggreenberg@somethingdigital.com>
2019-08-14 22:58:59 -0400

    Add temporary comment test

---
+++ .github/workflows/scripts/pr-comment.js
@@ -0,0 +1,15 @@
+const comment = require('github-comment');
+require('dotenv').config();
+
+const {
+    GITHUB_AUTH_TOKEN,
+    GITHUB_REPOSITORY,
+    GITHUB_EVENT_PATH,
+} = process.env;
+
+console.log(GITHUB_EVENT_PATH);
+// get file and show contents
+
+comment(GITHUB_AUTH_TOKEN, GITHUB_REPOSITORY, 16, 'This is a mother fucking comment.')
+    .then(response => console.log(response))
+    .catch(error => console.log(error))
