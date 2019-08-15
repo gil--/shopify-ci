20df18d05f5bc86e35e0456de8ae1a2b273f93f8
Gil Greenberg <ggreenberg@somethingdigital.com>
2019-08-14 22:18:41 -0400

    Add scripts to .github folder

---
+++ .github/workflows/scripts/theme-zip.js
@@ -0,0 +1,29 @@
+// Credit: Code extracted from Shopify Slate-Tools
+const fs = require('fs');
+const path = require('path');
+const archiver = require('archiver');
+
+const zipName = 'theme';
+const zipPath = path.join(process.cwd(), `${zipName}.zip`);
+const output = fs.createWriteStream(zipPath);
+const archive = archiver('zip');
+
+output.on('close', () => {
+    console.log(`✅ ${path.basename(zipPath)}: ${archive.pointer()} total bytes`);
+});
+
+archive.on('warning', (err) => {
+    if (err.code === 'ENOENT') {
+        console.log(err);
+    } else {
+        throw err;
+    }
+});
+
+archive.on('error', (err) => {
+    throw err;
+});
+
+archive.pipe(output);
+archive.directory(path.join(process.cwd(), 'src'), '/');
+archive.finalize();
