4ad9750b27df084ab5aadc3ddfa1fbab78b0dab0
Gil Greenberg <gilgreenberg@gmail.com>
2019-08-20 23:16:19 -0400

    Did token name change??

--- .github/workflows/scripts/lib/pr-comment.js
+++ .github/workflows/scripts/lib/pr-comment.js
@@ -3,17 +3,17 @@ const fs = require('fs');
 require('dotenv').config();
 
 const {
-    GITHUB_AUTH_TOKEN,
+    GITHUB_TOKEN,
     GITHUB_REPOSITORY,
 } = process.env;
 
 // Make a 💫
 module.exports.makeAComment = async ({ number, message }) => {
-    console.log(GITHUB_AUTH_TOKEN);
+    console.log(GITHUB_TOKEN);
     console.log(GITHUB_REPOSITORY);
     console.log(number);
     if (number != null) {
-        await comment(GITHUB_AUTH_TOKEN, GITHUB_REPOSITORY, number, message)
+        await comment(GITHUB_TOKEN, GITHUB_REPOSITORY, number, message)
             .then(response => console.log(response))
             .catch(error => console.log(error))
     }
