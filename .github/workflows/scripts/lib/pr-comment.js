25d6a86cdfbd37c43661d2bb30033f329127585d
Gil Greenberg <gilgreenberg@gmail.com>
2019-08-20 23:13:49 -0400

    more debug

--- .github/workflows/scripts/lib/pr-comment.js
+++ .github/workflows/scripts/lib/pr-comment.js
@@ -9,6 +9,9 @@ const {
 
 // Make a 💫
 module.exports.makeAComment = async ({ number, message }) => {
+    console.log(GITHUB_AUTH_TOKEN);
+    console.log(GITHUB_REPOSITORY);
+    console.log(number);
     if (number != null) {
         await comment(GITHUB_AUTH_TOKEN, GITHUB_REPOSITORY, number, message)
             .then(response => console.log(response))
