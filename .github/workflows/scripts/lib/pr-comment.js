6cd2735cd77b6ff987a6b1b8bc26b8a1fbac55a8
Gil Greenberg <gilgreenberg@gmail.com>
2019-08-20 22:54:23 -0400

    Use correct variable

--- .github/workflows/scripts/lib/pr-comment.js
+++ .github/workflows/scripts/lib/pr-comment.js
@@ -8,9 +8,9 @@ const {
 } = process.env;
 
 // Make a 💫
-module.exports.makeAComment = async ({ prNumber, message }) => {
-    if (content.number != null) {
-        await comment(GITHUB_AUTH_TOKEN, GITHUB_REPOSITORY, prNumber, message)
+module.exports.makeAComment = async ({ number, message }) => {
+    if (number != null) {
+        await comment(GITHUB_AUTH_TOKEN, GITHUB_REPOSITORY, number, message)
             .then(response => console.log(response))
             .catch(error => console.log(error))
     }
