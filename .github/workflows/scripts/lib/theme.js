6cea72588d4c7cec4acaef0a6f85940eaeb564fb
Gil Greenberg <gilgreenberg@gmail.com>
2019-08-25 12:25:28 -0400

    Add support for non-PR workflow by falling back to branch

--- .github/workflows/scripts/lib/theme.js
+++ .github/workflows/scripts/lib/theme.js
@@ -2,9 +2,16 @@ require('dotenv').config();
 
 const {
     GITHUB_SHA,
+    GITHUB_REF,
 } = process.env;
 
 module.exports.getThemeName = ({ prNumber }) => {
     const commitSha = GITHUB_SHA && GITHUB_SHA.substring(0, 6);
-    return `GITHUB-PR-${prNumber} ${commitSha}`;
+    console.log(GITHUB_REF);
+
+    if (prNumber) {
+        return `GITHUB-PR-${prNumber} ${commitSha}`;
+    } else {
+        return `GITHUB ${GITHUB_REF} ${commitSha}`;
+    }
 };
--- .github/workflows/scripts/theme-delete.js
+++ .github/workflows/scripts/theme-delete.js
@@ -8,7 +8,11 @@ const listTheme = async () => {
     const data = prData.getPrData();
     const newThemeName = theme.getThemeName({ prNumber: data.number });
     const themeNameRegex = `GITHUB-PR-${data.number}`;
-    console.log(data);
+
+    if (data.action !== 'synchronize') {
+        // we only need to delete PR theme when it's a PR synchronization
+        return;
+    }
 
     await shopifyClient.theme.list()
         .then(themes => {
