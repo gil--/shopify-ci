cce83e78630312e3932391645bbeb7a4f76ef224
Gil Greenberg <gilgreenberg@gmail.com>
2019-08-25 12:16:59 -0400

    Add debug

--- .github/workflows/scripts/theme-delete.js
+++ .github/workflows/scripts/theme-delete.js
@@ -8,15 +8,11 @@ const listTheme = async () => {
     const data = prData.getPrData();
     const newThemeName = theme.getThemeName({ prNumber: data.number });
     const themeNameRegex = `GITHUB-PR-${data.number}`;
-    console.log('data.number', data.number);
+    console.log(data);
 
     await shopifyClient.theme.list()
         .then(themes => {
             themes.map(existingTheme => {
-                console.log('existingTheme.name', existingTheme.name);
-                console.log('themeNameRegex', themeNameRegex);
-                console.log(existingTheme);
-
                 if (existingTheme.name.includes(themeNameRegex)) {
                     if (existingTheme.name !== newThemeName && existingTheme.role !== 'main') {
                         console.log(`Deleting ${existingTheme.name}`);
